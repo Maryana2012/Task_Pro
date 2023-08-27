d50d2f27024ae61fb81fa439d1eace82ac6ca51e
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-27 23:42:21 +0300

    Коментар для майбутніх маршрутів по таскам

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -5,7 +5,10 @@ const taskRouter = express.Router();
 
 taskRouter.post('/board', taskControllers.boardCreate);
 taskRouter.post('/board/:_id/column', taskControllers.columnCreate);
-
-
+// taskRouter.get('/tasks');
+// taskRouter.post('/tasks');
+// taskRouter.put('/tasks/${idTask}');
+// taskRouter.delete('/tasks/${idTask}');
+// taskRouter.patch('/tasks/${idTask}');
 
 export default taskRouter;
