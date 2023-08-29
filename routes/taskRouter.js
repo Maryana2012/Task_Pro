5d2db73690040238fc0fb69876dabbaff67af383
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-29 17:50:13 +0300

    Made routers tasks

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -4,11 +4,27 @@ import taskControllers from '../controllers/taskControllers.js'
 const taskRouter = express.Router();
 
 taskRouter.post('/board', taskControllers.boardCreate);
+
 taskRouter.post('/board/:_id/column', taskControllers.columnCreate);
-// taskRouter.get('/tasks');
-// taskRouter.post('/tasks');
-// taskRouter.put('/tasks/${idTask}');
-// taskRouter.delete('/tasks/${idTask}');
-// taskRouter.patch('/tasks/${idTask}');
+
+// Маршрут для отримання всіх тасків на дошці
+taskRouter.get("/", userValidators.authenticate, taskControllers.getAllTasks);
+
+// Маршрут для додавання нового таска на дошку
+taskRouter.post("/", userValidators.authenticate, taskControllers.addTask);
+
+// Маршрут для оновлення інформації про таск
+taskRouter.put("/:taskId", userValidators.authenticate, taskControllers.updateTask);
+
+// Маршрут для видалення таска з дошки
+taskRouter.delete("/:taskId", userValidators.authenticate, taskControllers.deleteTask);
+
+//  Маршрут для переміщення таски в іншу колонку
+taskRouter.patch("/:taskId/move/:newColumnId", userValidators.authenticate, taskControllers.moveTask);
+
+// Маршрут фільтрації тасок з певним пріоритетом на дошці
+taskRouter.get("/priority/:priority", userValidators.authenticate, taskControllers.getTasksByPriority);
+
+
 
 export default taskRouter;
