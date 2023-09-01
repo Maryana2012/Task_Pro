ad74c8a27bd902ca5052faf9fc4fe48f6d5eabf2
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-01 20:44:00 +0300

    Update backgroundController.js

--- controllers/backgroundController.js
+++ controllers/backgroundController.js
@@ -13,7 +13,7 @@ const getBackgroundPreviews = (req, res) => {
 
     res.status(200).json(previewData);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 };
 
