d59f8c4ff57f57ca28dc0dd8b3407d585ddfc306
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-01 15:00:40 +0300

    Create backgroundRoutes.js

---
+++ routes/backgroundRoutes.js
@@ -0,0 +1,11 @@
+// import express from "express";
+
+// import getBackgroundPreviews from '../controllers/backgroundController.js';
+
+// import userValidators from '../middlewars/user/userValidators.js'
+
+// const backgroundRouter = express.Router();
+
+// backgroundRouter.get('/', userValidators.authenticate, getBackgroundPreviews);
+
+// export default backgroundRouter;
