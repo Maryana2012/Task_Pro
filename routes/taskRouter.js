f4aa3cb49c397a758e036ba31dfa6af5e31e30bf
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-05-01 17:25:22 +0300

    update routes

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,18 +1,18 @@
 import express from 'express';
 
-import {taskSchema} from '../schemas/taskSchemas.js';
+import schema from '../schemas/taskSchemas.js';
 import taskControllers from '../controllers/taskControllers.js';
 import userValidators from '../middlewars/user/userValidators.js';
-import taskValidators from '../middlewars/tasks/taskValidators.js';
+// import taskValidators from '../middlewars/tasks/taskValidators.js';
 import { isEmptyBody } from '../middlewars/validatorEmptyBody.js';
 import { validateBody } from '../middlewars/validatorBody.js';
 const taskRouter = express.Router();
 
 taskRouter.get("/:boardId", userValidators.authenticate, taskControllers.getAllTasks);
 
-taskRouter.post("/", userValidators.authenticate, isEmptyBody, validateBody(taskSchema), taskControllers.addTask);
+taskRouter.post("/", userValidators.authenticate, isEmptyBody, validateBody(schema.taskSchema), taskControllers.addTask);
 
-taskRouter.put("/:taskId", userValidators.authenticate, isEmptyBody, validateBody(taskSchema), taskControllers.updateTask);
+taskRouter.put("/:taskId", userValidators.authenticate, isEmptyBody, validateBody(schema.taskSchema), taskControllers.updateTask);
 
 taskRouter.delete("/:taskId", userValidators.authenticate, taskControllers.deleteTask);
 
